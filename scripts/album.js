c54996ca92a0ef9514eff9b00c33c86ce4a5a29a
Kevin Voshell <kevinvoshell@gmail.com>
2017-01-05 01:31:15 -0600

    assignment 13 complete

--- scripts/album.js
+++ scripts/album.js
@@ -62,10 +62,19 @@ var setCurrentAlbum = function(album) {
 
 var findParentByClassName = function(element, targetClass) {
     if (element) {
+        
         var currentParent = element.parentElement;
+        if (currentParent === null){
+            alert("No parent found");
+        }
+        
         while (currentParent.className != targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;
+            if (currentParent.className === null) {
+                alert("No parent found with that class name");
+            }
         }
+        
         return currentParent;
     }
 };
